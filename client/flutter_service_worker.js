9fcaa9f169dc18f36e4a9b6b76fecc1f7f2a3dc2
jeanpaulM20 <jeanpaulM20@users.noreply.github.com>
2026-05-05 10:06:24 +0000

    deploy: 5e654a6beb0af51f7cb03e5fb9893308e57324c4

--- client/flutter_service_worker.js
+++ client/flutter_service_worker.js
@@ -6,7 +6,7 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {"assets/AssetManifest.json": "99914b932bd37a50b983c5e7c90ae93b",
 "assets/AssetManifest.bin.json": "a1fee2517bf598633e2f67fcf3e26c94",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
-"assets/fonts/MaterialIcons-Regular.otf": "dd4eaa4b4c177cd96cbbbba909d75b60",
+"assets/fonts/MaterialIcons-Regular.otf": "22bd222d810f0bc96ab5171d0dd1a168",
 "assets/AssetManifest.bin": "0b0a3415aad49b6e9bf965ff578614f9",
 "assets/NOTICES": "df4c57e1459f809aad65ccfefcf25691",
 "assets/FontManifest.json": "7b2a36307916a9721811788013e65289",
@@ -24,8 +24,8 @@ const RESOURCES = {"assets/AssetManifest.json": "99914b932bd37a50b983c5e7c90ae93
 "canvaskit/skwasm.js": "ac0f73826b925320a1e9b0d3fd7da61c",
 "push-sw.js": "0f259b92844f716341f37cf215ef4103",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "fa35a4942cb5a2e6337cf49ccf1649de",
-"main.dart.js": "8b2059c6a923c13af3d32b2a53a82199",
+"flutter_bootstrap.js": "b5411eac33a7bcb45ebbe3f5295090a7",
+"main.dart.js": "a8b58498f3e4316877d76b7236ec9ec2",
 "version.json": "c56a6c84bfb5e6e37f7f58da294a22b7",
 "push-notifications.js": "ec697b55a53574ab73beffd5e3f492d9"};
 // The application shell files that are downloaded before a service worker can
--- trainer/flutter_service_worker.js
+++ trainer/flutter_service_worker.js
@@ -25,7 +25,7 @@ const RESOURCES = {"assets/AssetManifest.json": "f534791734053e8f0aa77523db0292d
 "canvaskit/canvaskit.js": "26eef3024dbc64886b7f48e1b6fb05cf",
 "canvaskit/skwasm.js": "ac0f73826b925320a1e9b0d3fd7da61c",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "8ebf4dffd213d95a3a5e19fc8b16729b",
+"flutter_bootstrap.js": "4b2659f2dc5bef6533d6ef199f2eaf5b",
 "main.dart.js": "5a1ecef559a4c67e6d8e9ab9873c7ee3",
 "version.json": "df3b1bd3d5cffcfead453e6c90187824",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
