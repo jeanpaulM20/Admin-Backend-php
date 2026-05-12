ab5a4c5bf51966f1721ff8ef36baa607191b940b
jeanpaulM20 <jeanpaulM20@users.noreply.github.com>
2026-05-12 20:30:47 +0000

    deploy: ab84f92a361ad83884f24e533076e2f567980355

--- client/flutter_bootstrap.js
+++ client/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"82bd5b7209295a5b7ff8cae0df96e7870171e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4033239426"
+    serviceWorkerVersion: "2080193895"
   }
 });
--- client/flutter_service_worker.js
+++ client/flutter_service_worker.js
@@ -24,7 +24,7 @@ const RESOURCES = {"assets/AssetManifest.json": "99914b932bd37a50b983c5e7c90ae93
 "canvaskit/skwasm.js": "ac0f73826b925320a1e9b0d3fd7da61c",
 "push-sw.js": "0f259b92844f716341f37cf215ef4103",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "b11c7b8f068f22f240b2ef610e9c6f01",
+"flutter_bootstrap.js": "6dcdc4fa86c152e385edf87473271732",
 "main.dart.js": "69f5fa6c23847b2f0eb38425ebf74011",
 "version.json": "c56a6c84bfb5e6e37f7f58da294a22b7",
 "push-notifications.js": "ec697b55a53574ab73beffd5e3f492d9"};
