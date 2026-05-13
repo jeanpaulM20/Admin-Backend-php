388a916ef48fa45b74ec6f0a79ec598fedc262fc
jeanpaulM20 <jeanpaulM20@users.noreply.github.com>
2026-05-13 08:04:57 +0000

    deploy: d39bdadfad49125ec6975f191de10bbfbad0a50a

--- client/flutter_service_worker.js
+++ client/flutter_service_worker.js
@@ -24,7 +24,7 @@ const RESOURCES = {"assets/AssetManifest.json": "99914b932bd37a50b983c5e7c90ae93
 "canvaskit/skwasm.js": "ac0f73826b925320a1e9b0d3fd7da61c",
 "push-sw.js": "0f259b92844f716341f37cf215ef4103",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "d6a7ce5aa493f5905baace81d082f091",
+"flutter_bootstrap.js": "3b5a2f27ada3fffec8af217712357a17",
 "main.dart.js": "e6276e2663cdb16787d9eb97c9429818",
 "version.json": "c56a6c84bfb5e6e37f7f58da294a22b7",
 "push-notifications.js": "ec697b55a53574ab73beffd5e3f492d9"};
