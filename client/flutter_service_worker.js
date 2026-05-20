f854ed2f873d5ede45b3a238a412e82fd6b0dedf
jeanpaulM20 <jeanpaulM20@users.noreply.github.com>
2026-05-20 21:20:57 +0000

    deploy: 24e6a5487584e72d38927fe242ee2458398dd42e

--- client/flutter_service_worker.js
+++ client/flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"push-notifications.js": "ec697b55a53574ab73beffd5e3f492d9",
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "canvaskit/canvaskit.js": "26eef3024dbc64886b7f48e1b6fb05cf",
 "canvaskit/skwasm.wasm": "828c26a0b1cc8eb1adacbdd0c5e8bcfa",
-"flutter_bootstrap.js": "9fdce745803d9b4b5f1a69f297577794",
+"flutter_bootstrap.js": "eb4426ea6bb9fe99cd455c231a3f1578",
 "main.dart.js": "9c16e546d7a000cf7cebefdd7976e68e"};
 // The application shell files that are downloaded before a service worker can
 // start.
--- trainer/flutter_service_worker.js
+++ trainer/flutter_service_worker.js
@@ -29,9 +29,9 @@ const RESOURCES = {"assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "canvaskit/canvaskit.js": "26eef3024dbc64886b7f48e1b6fb05cf",
 "canvaskit/skwasm.wasm": "828c26a0b1cc8eb1adacbdd0c5e8bcfa",
-"flutter_bootstrap.js": "79de8d08a19aafa43d0a1adc738efcc0",
+"flutter_bootstrap.js": "ea2f48b90ac500e0d5337b4918448f88",
 "manifest.json": "d6094a571abab7e82ff0aa0514519e2c",
-"main.dart.js": "093c50e08fc991e8496babea49d037c3"};
+"main.dart.js": "df0d1e034704eb15350894a22cca0475"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
