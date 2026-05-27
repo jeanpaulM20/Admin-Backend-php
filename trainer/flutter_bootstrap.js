c7a1be3371a283a72aa5e5de293bc1e6ab7456c2
jeanpaulM20 <jeanpaulM20@users.noreply.github.com>
2026-05-27 09:21:17 +0000

    deploy: 7ce75a1dd03e83d29b6f24858774aa7f98113edb

--- trainer/flutter_bootstrap.js
+++ trainer/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"82bd5b7209295a5b7ff8cae0df96e7870171e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "145552957"
+    serviceWorkerVersion: "2746834566"
   }
 });
--- trainer/flutter_service_worker.js
+++ trainer/flutter_service_worker.js
@@ -30,9 +30,9 @@ const RESOURCES = {"assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8
 "canvaskit/canvaskit.js": "26eef3024dbc64886b7f48e1b6fb05cf",
 "canvaskit/skwasm.wasm": "828c26a0b1cc8eb1adacbdd0c5e8bcfa",
 "apple-touch-icon.png": "962354807350bed39a2cbebdc1951bca",
-"flutter_bootstrap.js": "2feecb6bed2ee3d532be6ae56870ccb0",
+"flutter_bootstrap.js": "219cddd97b385d96fb172dbcf231d039",
 "manifest.json": "e68ae8afcae584e181eea11ce5c4b9fe",
-"main.dart.js": "fa1e1d21d5ac9de4c0501785813c374b"};
+"main.dart.js": "94ab9a8b4a2248c5d1eb1b4a957f16d5"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
