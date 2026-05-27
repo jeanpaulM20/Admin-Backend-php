7210c4097e0307cf1057aaba0d17719b8bf399bb
jeanpaulM20 <jeanpaulM20@users.noreply.github.com>
2026-05-27 20:15:29 +0000

    deploy: 55c1461799c78101c9849756c0f0898866dbe850

--- client/flutter_service_worker.js
+++ client/flutter_service_worker.js
@@ -32,7 +32,7 @@ const RESOURCES = {"push-notifications.js": "ec697b55a53574ab73beffd5e3f492d9",
 "canvaskit/canvaskit.js": "26eef3024dbc64886b7f48e1b6fb05cf",
 "canvaskit/skwasm.wasm": "828c26a0b1cc8eb1adacbdd0c5e8bcfa",
 "apple-touch-icon.png": "962354807350bed39a2cbebdc1951bca",
-"flutter_bootstrap.js": "59db36b8971e4c1e82ca57ad388fde5f",
+"flutter_bootstrap.js": "0ae4965ee80de174745df624b40059df",
 "manifest.json": "ddaf03c153bcc1c1340ecd23d5c0f585",
 "main.dart.js": "14740fdc02e3ffaaa46d48cd170e5943"};
 // The application shell files that are downloaded before a service worker can
