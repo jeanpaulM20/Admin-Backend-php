41c58e92d6eb34c76eed7751c78374e59a17ffea
jeanpaulM20 <jeanpaulM20@users.noreply.github.com>
2026-05-30 06:12:14 +0000

    deploy: 96d7dca3464a9fc28d15a95d1254f9aa0a1ba5b5

--- trainer/main.dart.js
+++ trainer/main.dart.js
@@ -106231,7 +106231,7 @@ q=A.m("Keine \xdcbungen gefunden",n,n,n,n,A.d9().$3$color$fontSize$fontWeight(B.
 p=o.y.length!==0?"Versuch einen anderen Suchbegriff":"\xc4ndere die Filtereinstellungen"
 return A.a7(A.a([B.OB,s,B.ax,q,B.bL,A.m(p,n,n,n,n,A.al().$2$color$fontSize(A.t(153,B.j.gi(0)>>>16&255,B.j.gi(0)>>>8&255,B.j.gi(0)&255),12),n,n,n),B.bk,o.RM()],t.p),B.k,B.e,B.i)}return A.mp(a,new A.aDm(o,r),s+1,B.WX,n,B.S)},
 ada(a){var s,r,q,p,o=null,n=A.P(14)
-n=A.Dn(n,A.b0Q(A.jO(u.z,"/api/","")+"/api/exercise/"+a.a+"/icon.png",new A.aDj(this,a),B.ua,80,80),B.bH)
+n=A.Dn(n,A.b0Q(A.jO(u.z,"/api/","")+"/api/exercise/"+a.a+"/icon.png",new A.aDj(this,a),B.ua,100,100),B.bH)
 s=A.m(a.b,1,B.ak,o,o,A.al().$3$color$fontSize$fontWeight(B.I,14,B.Z),o,o,o)
 r=a.x
 r=r==null?o:r.b
@@ -106551,7 +106551,7 @@ A.aDj.prototype={
 $3(a,b,c){var s,r=null,q=this.a,p=this.b.e,o=q.JW(p)
 o=A.t(30,o.gi(0)>>>16&255,o.gi(0)>>>8&255,o.gi(0)&255)
 s=A.P(14)
-return A.a2(r,A.bC(q.WG(p),q.JW(p),r,32),B.l,r,r,new A.a1(o,r,r,s,r,r,B.q),r,80,r,r,r,r,r,80)},
+return A.a2(r,A.bC(q.WG(p),q.JW(p),r,32),B.l,r,r,new A.a1(o,r,r,s,r,r,B.q),r,100,r,r,r,r,r,100)},
 $S:681}
 A.aDk.prototype={
 $1(a){return a!=null&&a.length!==0},
