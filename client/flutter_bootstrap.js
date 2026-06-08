d3c024455a9ebec86442fe74758963cda7edfab9
jeanpaulM20 <jeanpaulM20@users.noreply.github.com>
2026-06-08 11:34:07 +0000

    deploy: def7ec9a1b88602b49a05df8b0a0ffb2669ac79c

--- client/flutter_bootstrap.js
+++ client/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"82bd5b7209295a5b7ff8cae0df96e7870171e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1042084713"
+    serviceWorkerVersion: "3499762230"
   }
 });
